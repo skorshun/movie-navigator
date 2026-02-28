eacf0ffdaffebc0b4fa45147d5441a8ea22e5866
Sergey Korshun <sergey.korshun@proton.me>
2026-02-28 15:25:48 +0200

    feat: add skeleton loading

---
+++ src/components/SkeletonCard.jsx
@@ -0,0 +1,10 @@
+const SkeletonCard = () => (
+    <div className="movie-card">
+        <div className="skeleton skeleton-poster"></div>
+        <div className="movie-info">
+            <div className="skeleton skeleton-text"></div>
+        </div>
+    </div>
+);
+
+export default SkeletonCard
